894dd31341eced1fa7a9541065c70f814240a47c
Huynh Ngoc My <myhn@blockchainlabs.asia>
2019-10-31 11:09:35 +0700

    Update ex2

--- My/Exercise2/src/component/Cart.js
+++ My/Exercise2/src/component/Cart.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-class Cart extends Component {
+class Item extends Component {
     render() {
-        let localItem= JSON.parse(localStorage.getItem("List"))
         return (
             <div>
-                <h5>Total: ${localItem["total"]}</h5>
-                <a href="/">Back to store</a>
+                <h5>{this.props.infor.name} {this.props.infor.model} {this.props.infor.price}</h5>
             </div>
         );
+    }
+}
+
+class Cart extends Component {
+    constructor(props){
+
+        super(props);
+        let item = JSON.parse(localStorage.getItem("List"))
+        for( var i in item.list){
+            if(item.list[i].status==="Add to cart"){
+                delete item.list[i]
+            }
+        }
+            this.state={
+                added: item.list,
+                total: item.total
+            }
+    }
+    render() {
+        return (
+            <div>
+                {   
+                this.state.added.map((ele,index) => <Item key={ele.name} infor={ele}  />)
+                }
+                <p>Total: ${this.state.total}</p>
+                <a href="/">Back to store</a>
+            </div>
+            
+        )
     }
 }
 

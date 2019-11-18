b6cd2c6243d264bb6ce4823c25225af9764289c0
Huynh Ngoc My <myhn@blockchainlabs.asia>
2019-11-18 18:01:50 +0700

    delete all

--- My/backend_API/server/controllers/product.controller.js
+++
@@ -1,9 +0,0 @@
-var Product = require('../models/product.model')
-
-module.exports.index=function(req, res,) {
-     Product.find().then(function(products){
-     //   res.render('index',{products:products})
-        res.status(200).send(products)
-        console.log(Product)
-     });
-}
